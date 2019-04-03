2080bbe1fdc31d7a2cb4339ba7d16ab095fe9956
Kumar <skuma395@LAMU02TC0MNGTFM.uhc.com>
2019-04-03 12:00:27 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppComponent } from './app.component';
 import { HeadModule } from './head/head.module';
 import { SharedModule } from './shared/shared.module';
 import { CommonUtilsModule } from './common-utils/common-utils.module';
 import { RestModule } from './rest/rest.module';
 import { PipesModule } from './pipes/pipes.module';
+import { DonutChartComponent } from './components/d3-objects/donut-chart/donut-chart.component';
+import { BarChartComponent } from './components/d3-objects/bar-chart/bar-chart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DonutChartComponent, BarChartComponent],
   imports: [
     BrowserModule,
     CommonModule,
@@ -28,4 +27,4 @@ import { PipesModule } from './pipes/pipes.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/d3-objects/donut-chart/donut-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donut-chart',
+  templateUrl: './donut-chart.component.html',
+  styleUrls: ['./donut-chart.component.less']
+})
+export class DonutChartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
