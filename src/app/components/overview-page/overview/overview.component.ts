8e95059ad2a534239076786a24fb2f8c2c346a35
Vashista <ivashist@ms.ds.uhc.com>
2019-04-04 19:52:13 +0530

    added  commons header footer card

--- src/app/components/overview-page/overview/overview.component.ts
+++ src/app/components/overview-page/overview/overview.component.ts
@@ -8,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class OverviewComponent implements OnInit {
   private pageTitle: String = '';
   private pagesubTitle: String = '';
+
   constructor() {
     this.pageTitle = 'Hello, Anne.';
     this.pagesubTitle = 'Your Insights at a glance.';
   }
+
   ngOnInit() {}
 }
