0bb411eacadf076f281fbb022d2b84fd0bf47e02
Kumar <akuma659@ms.ds.uhc.com>
2019-04-05 15:24:32 +0530

    first time adding hamburger menu in feature branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppComponent } from './app.component';
 import { HeadModule } from './head/head.module';
 import { SharedModule } from './shared/shared.module';
 import { CommonUtilsModule } from './common-utils/common-utils.module';
 import { RestModule } from './rest/rest.module';
 import { PipesModule } from './pipes/pipes.module';
+import { MatExpansionModule } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     CommonModule,
@@ -23,9 +21,10 @@ import { PipesModule } from './pipes/pipes.module';
     SharedModule,
     CommonUtilsModule,
     RestModule,
-    PipesModule
+    PipesModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/head/hamburger-menu/hamburger-menu.component.ts
+++ src/app/head/hamburger-menu/hamburger-menu.component.ts
@@ -1,23 +1,72 @@
-import { Component, AfterViewInit, HostListener } from '@angular/core';
-import {BreakpointObserver} from '@angular/cdk/layout';
+import { Component, AfterViewInit, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-hamburger-menu',
   templateUrl: './hamburger-menu.component.html',
   styleUrls: ['./hamburger-menu.component.less']
 })
-export class HamburgerMenuComponent {
+export class HamburgerMenuComponent implements AfterViewInit {
   public mobileQuery: boolean;
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+  public healthSystemName = 'North Region Health System';
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  /*** Array of Navigation Category List ***/
+  public navCategories = [
+    { icon: '/src/assets/images/icons/Action/round-home-24px.svg', name: 'Overview', path: '/' },
+    {
+      icon: '/src/assets/images/icons/Action/round-home-24px.svg',
+      name: 'Getting Reimbursed',
+      children: [
+        { name: 'Summary', path: '#' },
+        { name: 'Payments', path: '#' },
+        { name: 'Non-Payments', path: '#' },
+        { name: 'Appelas', path: '#' },
+        { name: 'Payment Integrity', path: '#' }
+      ]
+    },
+    {
+      icon: '/src/assets/images/icons/Action/round-home-24px.svg',
+      name: 'Care Delivery',
+      children: [{ name: 'Prior Authorizations', path: '#' }, { name: 'Patient Care Opportunity', path: '#' }]
+    },
+    {
+      icon: '/src/assets/images/icons/Action/round-home-24px.svg',
+      name: 'Issue Resolution',
+      children: [{ name: 'Summary', path: '#' }]
+    }
+  ];
+
+  fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {
     this.mobileQuery = this.breakpointObserver.isMatched('(max-width: 1024px)');
-
   }
 
   @HostListener('window:resize', ['$event'])
-    onResize() {
-      this.mobileQuery = this.breakpointObserver.isMatched('(max-width: 1024px)');
+  onResize() {
+    this.mobileQuery = this.breakpointObserver.isMatched('(max-width: 1024px)');
   }
 
+  /*** used to apply the CSS for dynamically generated elements ***/
+  public ngAfterViewInit(): void {
+    const listItems = this.elementRef.nativeElement.querySelectorAll('.mat-list-item') as HTMLElement[];
+    const listItemContents = this.elementRef.nativeElement.querySelectorAll('.mat-list-item-content') as HTMLElement[];
+    const listItemBody = this.elementRef.nativeElement.querySelectorAll('.mat-expansion-panel-body') as HTMLElement[];
+    listItemContents.forEach(listItem => {
+      this.renderer.setStyle(listItem, 'padding', '0px');
+      this.renderer.setStyle(listItem, 'height', 'auto');
+    });
+    listItems.forEach(listItem => {
+      this.renderer.setStyle(listItem, 'height', 'auto');
+      this.renderer.setStyle(listItem, 'padding', '8px 12px 8px 65px');
+      this.renderer.setStyle(listItem, 'width', 'auto');
+    });
+    listItemBody.forEach(listItem => {
+      this.renderer.setStyle(listItem, 'padding', '0px');
+    });
+  }
 }
