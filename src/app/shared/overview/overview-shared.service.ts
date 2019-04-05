bb33786f57f598de92ffdef8115d0966a14bf412
Mounika <gmounik7@ms.ds.uhc.com>
2019-04-05 16:46:41 +0530

    working on data format

--- src/app/shared/overview/overview-shared.service.ts
+++ src/app/shared/overview/overview-shared.service.ts
@@ -1,16 +1,54 @@
 import { Injectable } from '@angular/core';
 import { OverviewService } from '../../rest/overview/overview.service';
 import { OverviewPageModule } from '../../components/overview-page/overview-page.module';
+import { ArrayType } from '@angular/compiler';
 
 @Injectable({
   providedIn: OverviewPageModule
 })
 export class OverviewSharedService {
+  private overviewPageData: ArrayType;
   constructor(private overviewService: OverviewService) {}
   public getOverviewData() {
+    let tempArray: ArrayType;
+    let cPriorAuth;
+    let cSelfService;
+    let cPcor;
+    let cIR;
+    let ssoppurtunities;
     this.overviewService.getOverviewDataJson().subscribe(data => {
-      console.log(data);
-      return data;
+      if (
+        data[0].hasOwnProperty('PriorAuth') &&
+        data[0].PriorAuth.hasOwnProperty('LineOfBusiness') &&
+        data[0].PriorAuth.LineOfBusiness.hasOwnProperty('All') &&
+        data[0].PriorAuth.LineOfBusiness.All.hasOwnProperty('PriorAuthApprovedCount') &&
+        data[0].PriorAuth.LineOfBusiness.All.hasOwnProperty('PriorAuthNotApprovedCount') &&
+        data[0].PriorAuth.LineOfBusiness.All.hasOwnProperty('PriorAuthPendingCount') &&
+        data[0].PriorAuth.LineOfBusiness.All.hasOwnProperty('PriorAuthCancelledCount')
+      ) {
+        let priorAuthRequested =
+          data[0].PriorAuth.LineOfBusiness.All.PriorAuthApprovedCount +
+          data[0].PriorAuth.LineOfBusiness.All.PriorAuthNotApprovedCount +
+          data[0].PriorAuth.LineOfBusiness.All.PriorAuthPendingCount +
+          data[0].PriorAuth.LineOfBusiness.All.PriorAuthCancelledCount;
+        let approvedRate = data[0].PriorAuth.LineOfBusiness.All.PriorAuthApprovedCount / priorAuthRequested;
+
+        cPriorAuth = [
+          {
+            category: 'card',
+            type: 'donut',
+            data: {
+              title: 'Prior Authorization Approval',
+              cData: (approvedRate * 100).toFixed(1) + '%',
+              arrow: 'uparrow',
+              timeperiod: 'Rolling 12 Months'
+            }
+          }
+        ];
+      } else {
+        cPriorAuth = [{ category: 'card', type: 'donut', data: null }];
+      }
     });
+    return this.overviewPageData;
   }
 }
