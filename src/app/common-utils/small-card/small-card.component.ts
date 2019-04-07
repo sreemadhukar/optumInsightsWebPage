1890ca25cc5050c65ae3e86cc0ef85bbf7870513
Vashista <ivashist@ms.ds.uhc.com>
2019-04-07 23:59:29 +0530

    working on additional funtionality of small card

--- src/app/common-utils/small-card/small-card.component.ts
+++ src/app/common-utils/small-card/small-card.component.ts
@@ -11,6 +11,22 @@ export class SmallCardComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.data = this.card;
+    // this.data = this.card;
+    this.data = {
+      category: 'small-cards',
+      type: 'donutWithTrend',
+      title: 'Claims Yield',
+      data: {
+        cValues: [],
+        cData: '',
+        color: [{ color1: '#00A8F7' }, { color2: '#F5F5F5' }, { color3: '#FFFFFF' }],
+        gdata: []
+      },
+      sdata: {
+        sign: 'up',
+        data: '+2.3%'
+      },
+      timeperiod: 'Timeperiod - Rolling 12 Months'
+    };
   }
 }
