b392d0a91e35cc917ce6fff0eb5a948634d909fc
Kumar, Sparsh <sparsh.kumar@optum.com>
2019-04-10 08:33:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/api/getJwt', cors(), function(req, res) {
   });
 });
 
-app.get('*', function(req, res) {
+app.get('*', function(req, res) { 
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
