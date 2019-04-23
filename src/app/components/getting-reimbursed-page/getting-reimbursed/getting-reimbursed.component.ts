75716997545c58e77474d3f8f893f78f635dbd78
Vashista <ivashist@ms.ds.uhc.com>
2019-04-23 17:37:39 +0530

    designed JSON structure corresponding to Donut

--- src/app/components/getting-reimbursed-page/getting-reimbursed/getting-reimbursed.component.ts
+++ src/app/components/getting-reimbursed-page/getting-reimbursed/getting-reimbursed.component.ts
@@ -94,6 +94,55 @@ export class GettingReimbursedComponent implements OnInit {
         },
         labels: ['Medicare', 'Retirement'],
         timeperiod: 'Timeperiod - Rolling 6 Months'
+      },
+      {
+        category: 'medium-card',
+        type: 'donutWithTrendWithFooter',
+        title: 'Claims Non-Payment Rate',
+        data: {
+          graphValues: [0.4, 1 - 0.4],
+          centerNumber: '50.4%',
+          color: ['#3381FF', '#F5F5F5'],
+          gdata: ['card-inner', 'selfServiceCardD3Donut'],
+          sdata: [
+            {
+              sign: 'down',
+              data: '-1.3%'
+            }
+          ]
+        },
+        labels: ['Medicare', 'Retirement'],
+        footerInformation: [
+          {
+            Admin: 26,
+            Clinical: 27
+          }
+        ],
+        timeperiod: 'Timeperiod - Rolling 6 Months'
+      },
+      {
+        category: 'medium-card',
+        type: 'donutWithTrendAdditionalInformation',
+        title: 'Claims Non-Payment Rate',
+        data: {
+          graphValues: [0.4, 1 - 0.4],
+          centerNumber: '50.4%',
+          color: ['#3381FF', '#F5F5F5'],
+          gdata: ['card-inner', 'selfServiceCardD3Donut'],
+          sdata: [
+            {
+              sign: 'down',
+              data: '-1.3%'
+            }
+          ]
+        },
+        additionalInformation: [
+          {
+            AdminStandard: 3,
+            Urgent: 18
+          }
+        ],
+        timeperiod: 'Timeperiod - Rolling 6 Months'
       }
     ];
   }
