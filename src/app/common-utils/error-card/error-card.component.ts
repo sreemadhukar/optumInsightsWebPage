1b2d6bc9642ed2bd01099f853d12dbcabbb36949
Mounika <gmounik7@ms.ds.uhc.com>
2019-04-26 19:07:09 +0530

    Designing of error card is done

--- src/app/common-utils/error-card/error-card.component.ts
+++ src/app/common-utils/error-card/error-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-error-card',
@@ -7,14 +9,20 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ErrorCardComponent implements OnInit {
   public errorCodes = [
-    { code: '501', title: 'Data Could Not Be Retrieved', message: 'Sorry. Please try again later' },
-    { code: '503', message: '' },
-    { code: '500', message: '' },
-    { code: '404', title: 'Data Not Available', message: 'Sorry. Please change your filter settings' },
-    { code: '403', message: '' }
+    { code: 501, title: 'Data Could Not Be Retrieved', message: 'Sorry. Please try again later' },
+    { code: 503, message: '' },
+    { code: 500, message: '' },
+    { code: 404, title: 'Data Not Available', message: 'Sorry. Please change your filter settings' },
+    { code: 403, message: '' }
   ];
   @Input() data;
-  constructor() {}
+  constructor(private iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    /** INITIALIZING SVG ICONS TO USE IN DESIGN - ANGULAR MATERIAL */
+    iconRegistry.addSvgIcon(
+      'help',
+      sanitizer.bypassSecurityTrustResourceUrl('/src/assets/images/icons/Action/baseline-help_outline-24px.svg')
+    );
+  }
   ngOnInit() {
     console.log(this.data);
   }
