c459dca07b1f342df7a1aa143dbf25d8b34deeb9
Rajan <rrajan20@ms.ds.uhc.com>
2019-04-29 21:58:58 +0530

    help icon added in main cards

--- src/app/common-utils/card/card.component.ts
+++ src/app/common-utils/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import { GlossaryExpandService } from '../../shared/glossary-expand.service';
 
 @Component({
   selector: 'app-card',
@@ -13,13 +14,21 @@ export class CardComponent implements OnInit {
   widthDonut: Number = 234;
   heightRotatingArrow: Number = 212;
   widthRotatingArrow: Number = 225;
-  constructor(private iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    private glossaryExpandService: GlossaryExpandService
+  ) {
     /** INITIALIZING SVG ICONS TO USE IN DESIGN - ANGULAR MATERIAL */
 
     iconRegistry.addSvgIcon(
       'help',
       sanitizer.bypassSecurityTrustResourceUrl('/src/assets/images/icons/Action/baseline-help_outline-24px.svg')
     );
+  }
+
+  helpIconClick(title) {
+    this.glossaryExpandService.setMessage(title);
   }
   ngOnInit() {}
 }
