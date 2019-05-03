2b817221653cedab2afeb1dcd5bdcac45a338d00
Vashista <ivashist@ms.ds.uhc.com>
2019-05-03 12:30:07 +0530

    updated environemtn file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,10 @@
  * MAINTAIN ALPHABETIC ORDER
  */
 export const environment: any = {
+  /*
+  apiProxyUrl: 'https://gateway-stage-core.optum.com/api/qaone/pdr/uhci/v1/',
+  originUrl: 'https://pedui1-pedtst.ocp-ctc-core-nonprod.optum.com/',
+  */
   apiProxyUrl: 'https://gateway-stage-core.optum.com/api/int/pdr/uhci/v1/',
   originUrl: 'https://pedintui-pedstg.ocp-ctc-dmz-stg.optum.com/',
   apiUrls: {
