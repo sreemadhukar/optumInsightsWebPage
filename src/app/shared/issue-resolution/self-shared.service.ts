c6c9bdaeb16218e388d32e747e4ba03be4dcb310
Vashista <ivashist@ms.ds.uhc.com>
2019-05-08 19:08:29 +0530

    working on Self Service Data binding and comments

--- src/app/shared/issue-resolution/self-shared.service.ts
+++ src/app/shared/issue-resolution/self-shared.service.ts
@@ -46,139 +46,255 @@ export class SelfSharedService {
             providerSystems.SelfServiceInquiries.ALL.hasOwnProperty('Utilizations')
           ) {
             const utilization = providerSystems.SelfServiceInquiries.ALL.Utilizations;
+            try {
+              adoptionRate = {
+                category: 'app-card',
+                type: 'donut',
+                title: 'Self-Service Adoption Rate',
+                data: {
+                  graphValues: [
+                    utilization.OverallLinkAdoptionRate * 100,
+                    100 - utilization.OverallLinkAdoptionRate * 100
+                  ],
+                  centerNumber: (utilization.OverallLinkAdoptionRate * 100).toFixed(0) + ' %',
+                  color: ['#3381FF', '#D7DCE1'],
+                  gdata: ['card-inner', 'selfAdoptionRate'],
+                  sdata: {
+                    sign: 'down',
+                    data: '-1.3%'
+                  }
+                },
+                timeperiod: this.timeFrame
+              };
+            } catch (Error) {
+              adoptionRate = {
+                category: 'app-card',
+                type: 'donut',
+                title: null,
+                data: null,
+                sdata: null,
+                timeperiod: null
+              };
+            } // End try catch for Adoption Rate
+            try {
+              linkEdiRation = {
+                category: 'app-card',
+                type: 'donut',
+                title: 'LINK & EDI to Call Ratio',
+                data: {
+                  graphValues: [100 - utilization.LinkAdoptionRate * 100, utilization.LinkAdoptionRate * 100],
+                  centerNumber: (utilization.LinkAdoptionRate * 100).toFixed(0) + ' %',
+                  color: ['#3381FF', '#D7DCE1'],
+                  gdata: ['card-inner', 'linkAndEdiCallRatio'],
+                  sdata: {
+                    sign: 'up',
+                    data: '+1.3%'
+                  }
+                },
+                timeperiod: this.timeFrame
+              };
+            } catch (Error) {
+              linkEdiRation = {
+                category: 'app-card',
+                type: 'donut',
+                title: null,
+                data: null,
+                sdata: null,
+                timeperiod: null
+              };
+            } // End try catch for Link & EDI Ration
+            try {
+              paperLessDelivery = {
+                category: 'app-card',
+                type: 'donut',
+                title: 'Paperless Delivery',
+                data: {
+                  graphValues: [
+                    utilization.PaperAndPostageAdoptionRate * 100,
+                    100 - utilization.PaperAndPostageAdoptionRate * 100
+                  ],
+                  centerNumber: (utilization.PaperAndPostageAdoptionRate * 100).toFixed(0) + ' %',
+                  color: ['#3381FF', '#D7DCE1'],
+                  gdata: ['card-inner', 'paperlessDelivery'],
+                  sdata: {
+                    sign: 'down',
+                    data: '-3.7%'
+                  }
+                },
+                timeperiod: this.timeFrame
+              };
+            } catch (Error) {
+              paperLessDelivery = {
+                category: 'app-card',
+                type: 'donut',
+                title: null,
+                data: null,
+                sdata: null,
+                timeperiod: null
+              };
+            } // End try catch for PaperlessDelivery
+          } else {
             adoptionRate = {
               category: 'app-card',
               type: 'donut',
-              title: 'Self-Service Adoption Rate',
-              data: {
-                graphValues: [
-                  utilization.OverallLinkAdoptionRate * 100,
-                  100 - utilization.OverallLinkAdoptionRate * 100
-                ],
-                centerNumber: (utilization.OverallLinkAdoptionRate * 100).toFixed(0) + ' %',
-                color: ['#3381FF', '#D7DCE1'],
-                gdata: ['card-inner', 'selfAdoptionRate'],
-                sdata: {
-                  sign: 'down',
-                  data: '-1.3%'
-                }
-              },
-              timeperiod: this.timeFrame
+              title: null,
+              data: null,
+              sdata: null,
+              timeperiod: null
             };
+
             linkEdiRation = {
               category: 'app-card',
               type: 'donut',
-              title: 'LINK & EDI to Call Ratio',
-              data: {
-                graphValues: [100 - utilization.LinkAdoptionRate * 100, utilization.LinkAdoptionRate * 100],
-                centerNumber: (utilization.LinkAdoptionRate * 100).toFixed(0) + ' %',
-                color: ['#3381FF', '#D7DCE1'],
-                gdata: ['card-inner', 'linkAndEdiCallRatio'],
-                sdata: {
-                  sign: 'up',
-                  data: '+1.3%'
-                }
-              },
-              timeperiod: this.timeFrame
+              title: null,
+              data: null,
+              sdata: null,
+              timeperiod: null
             };
+
             paperLessDelivery = {
               category: 'app-card',
               type: 'donut',
-              title: 'Paperless Delivery',
-              data: {
-                graphValues: [
-                  utilization.PaperAndPostageAdoptionRate * 100,
-                  100 - utilization.PaperAndPostageAdoptionRate * 100
-                ],
-                centerNumber: (utilization.PaperAndPostageAdoptionRate * 100).toFixed(0) + ' %',
-                color: ['#3381FF', '#D7DCE1'],
-                gdata: ['card-inner', 'paperlessDelivery'],
-                sdata: {
-                  sign: 'down',
-                  data: '-3.7%'
-                }
-              },
-              timeperiod: this.timeFrame
+              title: null,
+              data: null,
+              sdata: null,
+              timeperiod: null
             };
-          } else {
-            console.log('Else Utilazation');
-          }
+          } // End if else block Utilization
           if (
             providerSystems.hasOwnProperty('SelfServiceInquiries') &&
             providerSystems.SelfServiceInquiries.hasOwnProperty('ALL') &&
             providerSystems.SelfServiceInquiries.ALL.hasOwnProperty('SelfService')
           ) {
             const selfService = providerSystems.SelfServiceInquiries.ALL.SelfService;
-
+            try {
+              saveStaffTime = {
+                category: 'app-card',
+                type: 'small-bar-chart',
+                title: "Save Your Staff's Time by",
+                data: {
+                  chartData: [
+                    { labelsRight: '8 hours/day', values: 8, metricName: 'Phone' },
+                    { labelsRight: '2 hours/day', values: 2, metricName: 'Self Service' }
+                  ],
+                  value: '6 hours/day',
+                  color: ['#80B0FF', '#3381FF'],
+                  gdata: ['card-inner', 'staffTimeSave']
+                },
+                timeperiod: this.timeFrame
+              };
+            } catch (Error) {
+              saveStaffTime = {
+                category: 'app-card',
+                type: 'small-bar-chart',
+                title: null,
+                data: null,
+                sdata: null,
+                timeperiod: null
+              };
+            } // End try catch for Save Your's Staff TIme
+            try {
+              reduceClaimProcessingTime = {
+                category: 'app-card',
+                type: 'small-bar-chart',
+                title: 'Reduce Claim Processing Time by',
+                data: {
+                  chartData: [
+                    {
+                      labelsRight: selfService.AveragePaperClaimProcessingTime.toFixed(0) + ' days',
+                      values: selfService.AveragePaperClaimProcessingTime.toFixed(0),
+                      metricName: 'Phone'
+                    },
+                    {
+                      labelsRight: selfService.AverageClaimProcessingTime.toFixed(0) + ' days',
+                      values: selfService.AverageClaimProcessingTime.toFixed(0),
+                      metricName: 'Self Service'
+                    }
+                  ],
+                  value:
+                    selfService.AveragePaperClaimProcessingTime.toFixed(0) -
+                    selfService.AverageClaimProcessingTime.toFixed(0) +
+                    ' days',
+                  color: ['#80B0FF', '#3381FF'],
+                  gdata: ['card-inner', 'reduceClaimTime']
+                },
+                timeperiod: this.timeFrame
+              };
+            } catch (Error) {
+              reduceClaimProcessingTime = {
+                category: 'app-card',
+                type: 'small-bar-chart',
+                title: null,
+                data: null,
+                sdata: null,
+                timeperiod: null
+              };
+            } // End try catch for Reduce Your Claim Processing Time
+            try {
+              reduceReconsiderationProcessing = {
+                category: 'app-card',
+                type: 'small-bar-chart',
+                title: 'Reduce Reconsideration Processing by:',
+                data: {
+                  chartData: [
+                    {
+                      labelsRight: selfService.AveragePaperReconsideredProcessingTime.toFixed(0) + ' days',
+                      values: selfService.AveragePaperReconsideredProcessingTime.toFixed(0),
+                      metricName: 'Phone'
+                    },
+                    {
+                      labelsRight: selfService.AverageReconsideredProcessingTime.toFixed(0) + ' days',
+                      values: selfService.AverageReconsideredProcessingTime.toFixed(0),
+                      metricName: 'Self Service'
+                    }
+                  ],
+                  value:
+                    selfService.AveragePaperReconsideredProcessingTime.toFixed(0) -
+                    selfService.AverageReconsideredProcessingTime.toFixed(0) +
+                    ' days',
+                  color: ['#80B0FF', '#3381FF'],
+                  gdata: ['card-inner', 'reduceProcessing']
+                },
+                timeperiod: this.timeFrame
+              };
+            } catch (Error) {
+              reduceReconsiderationProcessing = {
+                category: 'app-card',
+                type: 'small-bar-chart',
+                title: null,
+                data: null,
+                sdata: null,
+                timeperiod: null
+              };
+            } // End try Catch for Reduce Reconsideration Processing
+          } else {
             saveStaffTime = {
               category: 'app-card',
               type: 'small-bar-chart',
-              title: "Save Your Staff's Time by",
-              data: {
-                chartData: [
-                  { labelsRight: '8 hours/day', values: 8, metricName: 'Phone' },
-                  { labelsRight: '2 hours/day', values: 2, metricName: 'Self Service' }
-                ],
-                value: '6 hours/day',
-                color: ['#80B0FF', '#3381FF'],
-                gdata: ['card-inner', 'staffTimeSave']
-              },
-              timeperiod: this.timeFrame
+              title: null,
+              data: null,
+              sdata: null,
+              timeperiod: null
             };
+
             reduceClaimProcessingTime = {
               category: 'app-card',
               type: 'small-bar-chart',
-              title: 'Reduce Claim Processing Time by',
-              data: {
-                chartData: [
-                  {
-                    labelsRight: selfService.AveragePaperClaimProcessingTime.toFixed(0) + ' days',
-                    values: selfService.AveragePaperClaimProcessingTime.toFixed(0),
-                    metricName: 'Phone'
-                  },
-                  {
-                    labelsRight: selfService.AverageClaimProcessingTime.toFixed(0) + ' days',
-                    values: selfService.AverageClaimProcessingTime.toFixed(0),
-                    metricName: 'Self Service'
-                  }
-                ],
-                value:
-                  selfService.AveragePaperClaimProcessingTime.toFixed(0) -
-                  selfService.AverageClaimProcessingTime.toFixed(0) +
-                  ' days',
-                color: ['#80B0FF', '#3381FF'],
-                gdata: ['card-inner', 'reduceClaimTime']
-              },
-              timeperiod: this.timeFrame
+              title: null,
+              data: null,
+              sdata: null,
+              timeperiod: null
             };
+
             reduceReconsiderationProcessing = {
               category: 'app-card',
               type: 'small-bar-chart',
-              title: 'Reduce Reconsideration Processing by:',
-              data: {
-                chartData: [
-                  {
-                    labelsRight: selfService.AveragePaperReconsideredProcessingTime.toFixed(0) + ' days',
-                    values: selfService.AveragePaperReconsideredProcessingTime.toFixed(0),
-                    metricName: 'Phone'
-                  },
-                  {
-                    labelsRight: selfService.AverageReconsideredProcessingTime.toFixed(0) + ' days',
-                    values: selfService.AverageReconsideredProcessingTime.toFixed(0),
-                    metricName: 'Self Service'
-                  }
-                ],
-                value:
-                  selfService.AveragePaperReconsideredProcessingTime.toFixed(0) -
-                  selfService.AverageReconsideredProcessingTime.toFixed(0) +
-                  ' days',
-                color: ['#80B0FF', '#3381FF'],
-                gdata: ['card-inner', 'reduceProcessing']
-              },
-              timeperiod: this.timeFrame
+              title: null,
+              data: null,
+              sdata: null,
+              timeperiod: null
             };
-          } else {
-          }
+          } // End If Else block SelfService
           tempArray[0] = adoptionRate;
           tempArray[1] = linkEdiRation;
           tempArray[2] = paperLessDelivery;
@@ -190,7 +306,7 @@ export class SelfSharedService {
           resolve(this.selfServiceData);
         },
         err => {
-          console.log('Self service Error', err);
+          console.log('Self service Error Data', err);
         }
       ); // end subscribing to REST call
     }); // ends Promise
