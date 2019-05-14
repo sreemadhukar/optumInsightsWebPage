70cbf2b76c490b6b082575161c6e35f034bc9691
Vashista <ivashist@ms.ds.uhc.com>
2019-05-15 00:33:50 +0530

    updated phone text to Mail

--- src/app/shared/overview/overview-shared.service.ts
+++ src/app/shared/overview/overview-shared.service.ts
@@ -216,7 +216,7 @@ export class OverviewSharedService {
               color: ['#3381FF', '#FFFFFF', '#80B0FF'],
               graphValuesTitle: 'Avg. Transaction Costs',
               graphData1: 'for Self Service',
-              graphData2: 'for Phone Call',
+              graphData2: 'for Mail',
               gdata: ['card-structure', 'totalCallCost']
             }
           });
@@ -333,7 +333,7 @@ export class OverviewSharedService {
               color: ['#3381FF', '#FFFFFF', '#80B0FF'],
               graphValuesTitle: 'Avg. Processing Times',
               graphData1: 'for Self Service',
-              graphData2: 'for Phone Call',
+              graphData2: 'for Mail',
               gdata: ['card-structure', 'reduceReconsiderationProcessing']
             }
           });
