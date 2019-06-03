7f8a9d6702c0f716114f6b85b81fff26a9c88a7e
Gautam <pgautam7@ms.ds.uhc.com>
2019-06-03 16:51:02 +0530

    Appeals Overturned Rate Donut

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,7 +5,7 @@
  * MAINTAIN ALPHABETIC ORDER
  */
 export const environment: any = {
-  apiProxyUrl: 'https://gateway-stage-core.optum.com/api/devfour/pdr/uhci/v1/',
+  apiProxyUrl: 'https://gateway-stage-core.optum.com/api/int/pdr/uhci/v1/',
   originUrl: 'https://pedintui-pedstg.ocp-ctc-dmz-stg.optum.com/',
   apiUrls: {
     ExecutiveSummaryPath: 'providersystems/',
