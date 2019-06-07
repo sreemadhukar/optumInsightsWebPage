285ec52a69b13111dfa8fcb6484095727d50594e
Mounika <gmounik7@ms.ds.uhc.com>
2019-06-07 21:48:26 +0530

    Fixed design

--- src/app/common-utils/filter/filter.component.ts
+++ src/app/common-utils/filter/filter.component.ts
@@ -10,9 +10,11 @@ import { SessionService } from '../../shared/session.service';
 })
 export class FilterComponent implements OnInit {
   public lobData: string;
+  public arrowmark: boolean;
   public lobs = ['All', 'Community & State', 'Employee & Individual', 'Medicare & Retirement'];
   constructor(private iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private session: SessionService) {
-    this.lobData = session.lob;
+    this.lobData = this.session.lob;
+    this.arrowmark = false;
     iconRegistry.addSvgIcon(
       'arrowdn',
       sanitizer.bypassSecurityTrustResourceUrl('/src/assets/images/icons/Action/baseline-keyboard_arrow_down-24px.svg')
@@ -22,6 +24,16 @@ export class FilterComponent implements OnInit {
       sanitizer.bypassSecurityTrustResourceUrl('/src/assets/images/icons/Action/baseline-keyboard_arrow_up-24px.svg')
     );
   }
-
-  ngOnInit() {}
+  public clickArrowMark() {
+    this.arrowmark = !this.arrowmark;
+  }
+  ngOnInit() {
+    this.lobData = this.session.lob;
+  }
+  resetFilter() {
+    this.session.lob = this.lobData = this.lobs[0];
+  }
+  applyFilter() {
+    this.session.lob = this.lobData;
+  }
 }
