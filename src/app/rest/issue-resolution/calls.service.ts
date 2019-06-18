2a52091a53dc617642f8f413980b8dabc27c7484
Vashista <ivashist@ms.ds.uhc.com>
2019-06-19 00:03:47 +0530

    corrected format for the small-bar chart

--- src/app/rest/issue-resolution/calls.service.ts
+++ src/app/rest/issue-resolution/calls.service.ts
@@ -21,11 +21,10 @@ export class CallsService {
       Accept: '*/*'
     });
 
-    let eparams = new HttpParams();
-    eparams = eparams.append('filter', 'executive');
+    const params = new HttpParams();
     const executiveURL = this.APP_URL + this.CALLS_SERVICE_PATH + parameters[0];
     return combineLatest(
-      this.http.get(executiveURL, { params: eparams, headers: myHeader }).pipe(
+      this.http.get(executiveURL, { params, headers: myHeader }).pipe(
         retry(2),
         map(res => res),
         catchError(err => of(err))
