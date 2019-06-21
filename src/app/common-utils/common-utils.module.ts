11e4008b2a53e610ea74b7e0e7f5b90f987e2ae4
Vashista <ivashist@ms.ds.uhc.com>
2019-06-21 14:44:46 +0530

    working on print - componnt

--- src/app/common-utils/common-utils.module.ts
+++ src/app/common-utils/common-utils.module.ts
@@ -29,6 +29,7 @@ import { FilterComponent } from './filter/filter.component';
 import { MedBarChartComponent } from './d3-objects/med-bar-chart/med-bar-chart.component';
 import { MultiSelectComponent } from './multi-select/multi-select.component';
 import { DonutChartOnchangeComponent } from './d3-objects/donut-chart-onchange/donut-chart-onchange.component';
+import { PrintComponent } from './print/print.component';
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule, HeadMaterialModule, RouterModule],
   exports: [
@@ -52,7 +53,8 @@ import { DonutChartOnchangeComponent } from './d3-objects/donut-chart-onchange/d
     HighlightPipe,
     FilterComponent,
     MedBarChartComponent,
-    DonutChartComponent
+    DonutChartComponent,
+    PrintComponent
   ],
   declarations: [
     MiniTileComponent,
@@ -80,7 +82,8 @@ import { DonutChartOnchangeComponent } from './d3-objects/donut-chart-onchange/d
     FilterComponent,
     MedBarChartComponent,
     MultiSelectComponent,
-    DonutChartOnchangeComponent
+    DonutChartOnchangeComponent,
+    PrintComponent
   ],
   entryComponents: [ProviderSearchComponent]
 })
---
+++ src/app/common-utils/print/print.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-print',
+  templateUrl: './print.component.html',
+  styleUrls: ['./print.component.scss']
+})
+export class PrintComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
