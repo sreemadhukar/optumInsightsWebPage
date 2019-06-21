1c51f0bc6add0a4ae71ea2f7e1e02d5bd453c0c1
Vashista <ivashist@ms.ds.uhc.com>
2019-06-21 15:01:57 +0530

    working on print - componnt

--- src/app/common-utils/print/print.component.ts
+++ src/app/common-utils/print/print.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-print',
@@ -6,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./print.component.scss']
 })
 export class PrintComponent implements OnInit {
-  constructor() {}
+  constructor(private iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    /** INITIALIZING SVG ICONS TO USE IN DESIGN - ANGULAR MATERIAL */
+
+    iconRegistry.addSvgIcon(
+      'print-icon',
+      sanitizer.bypassSecurityTrustResourceUrl('/src/assets/images/icons/print-icon.svg')
+    );
+  }
 
   ngOnInit() {}
 }
