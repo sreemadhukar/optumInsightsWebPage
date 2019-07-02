8fd8214b1016ad3fdd6cbf0bbcc38efb96464e5a
Vashista <ivashist@ms.ds.uhc.com>
2019-07-02 16:29:16 +0530

    working on non-payment API

--- src/app/shared/getting-reimbursed/non-payment-shared.service.ts
+++ src/app/shared/getting-reimbursed/non-payment-shared.service.ts
@@ -11,6 +11,7 @@ import { AuthorizationService } from '../../auth/_service/authorization.service'
 export class NonPaymentSharedService {
   public providerKey;
   public summaryData: Array<object> = [];
+  public timeFrame: string;
   constructor(
     private nonPaymentService: NonPaymentService,
     private common: CommonUtilsService,
@@ -19,7 +20,7 @@ export class NonPaymentSharedService {
   ) {}
   public getNonPayment() {
     this.providerKey = this.session.providerKeyData();
-
+    this.timeFrame = this.session.timeFrame;
     return new Promise(resolve => {
       const parameters = [this.providerKey, 'NONPAYMENT_METRICS'];
       // let nonPayment: object;
@@ -28,6 +29,117 @@ export class NonPaymentSharedService {
         ([nonPaymentData1]) => {
           this.summaryData = nonPaymentData1;
           console.log('Shared Non Payment', nonPaymentData1);
+          let claimsNotPaid: Object;
+          let claimsNotPaidRate: Object;
+          if (
+            nonPaymentData1.hasOwnProperty('All') &&
+            nonPaymentData1.All != null &&
+            nonPaymentData1.All.hasOwnProperty('ClaimsLobSummary') &&
+            nonPaymentData1.All.ClaimsLobSummary.length &&
+            nonPaymentData1.All.ClaimsLobSummary[0].ClaimsDenied &&
+            nonPaymentData1.hasOwnProperty('Cs') &&
+            nonPaymentData1.Cs.hasOwnProperty('ClaimsLobSummary') &&
+            nonPaymentData1.Cs.ClaimsLobSummary.length &&
+            nonPaymentData1.Cs.ClaimsLobSummary[0].hasOwnProperty('ClaimsDenied') &&
+            nonPaymentData1.hasOwnProperty('Ei') &&
+            nonPaymentData1.Ei.hasOwnProperty('ClaimsLobSummary') &&
+            nonPaymentData1.Ei.ClaimsLobSummary.length &&
+            nonPaymentData1.Ei.ClaimsLobSummary[0].hasOwnProperty('ClaimsDenied') &&
+            nonPaymentData1.hasOwnProperty('Mr') &&
+            nonPaymentData1.Mr.hasOwnProperty('ClaimsLobSummary') &&
+            nonPaymentData1.Mr.ClaimsLobSummary.length &&
+            nonPaymentData1.Mr.ClaimsLobSummary[0].hasOwnProperty('ClaimsDenied')
+          ) {
+            const nonPaidData = [
+              nonPaymentData1.Mr.ClaimsLobSummary[0].ClaimsDenied,
+              nonPaymentData1.Cs.ClaimsLobSummary[0].ClaimsDenied,
+              nonPaymentData1.Ei.ClaimsLobSummary[0].ClaimsDenied
+            ];
+            claimsNotPaid = {
+              category: 'app-card',
+              type: 'donutWithLabel',
+              title: 'Claims Not Paid',
+              data: {
+                graphValues: nonPaidData,
+                centerNumber:
+                  this.common.nFormatter(nonPaymentData1.All.ClaimsLobSummary[0].ClaimsDenied) < 1 &&
+                  this.common.nFormatter(nonPaymentData1.All.ClaimsLobSummary[0].ClaimsDenied) > 0
+                    ? '< $1'
+                    : '$' + this.common.nFormatter(nonPaymentData1.All.ClaimsLobSummary[0].ClaimsDenied),
+                color: ['#3381FF', '#80B0FF', '#003DA1'],
+                gdata: ['card-inner', 'claimsNotPaid'],
+                sdata: {
+                  sign: 'down',
+                  data: '-10.2%'
+                },
+                labels: ['Medicare & Retirement', 'Community & State', 'Employer & Individual'],
+                hover: true
+              },
+              besideData: {
+                labels: ['Medicare & Retirement', 'Community & State', 'Employer & Individual'],
+                color: ['#3381FF', '#80B0FF', '#003DA1']
+              },
+              timeperiod: this.timeFrame
+            };
+          } else {
+            claimsNotPaid = {
+              category: 'app-card',
+              type: 'donutWithLabel',
+              title: null,
+              data: null,
+              besideData: null,
+              bottomData: null,
+              timeperiod: null
+            };
+          }
+          if (
+            nonPaymentData1.hasOwnProperty('All') &&
+            nonPaymentData1.All != null &&
+            nonPaymentData1.All.hasOwnProperty('ClaimsLobSummary') &&
+            nonPaymentData1.All.ClaimsLobSummary.length &&
+            nonPaymentData1.All.ClaimsLobSummary[0].hasOwnProperty('ClaimsYieldRate') &&
+            nonPaymentData1.All.ClaimsLobSummary[0].hasOwnProperty('ClaimsNonPaymentRate')
+          ) {
+            claimsNotPaidRate = {
+              category: 'app-card',
+              type: 'donut',
+              title: 'Claims Non-Payment Rate',
+              data: {
+                graphValues: [
+                  nonPaymentData1.All.ClaimsLobSummary[0].ClaimsNonPaymentRate,
+                  nonPaymentData1.All.ClaimsLobSummary[0].ClaimsYieldRate
+                ],
+                centerNumber:
+                  nonPaymentData1.All.ClaimsLobSummary[0].ClaimsNonPaymentRate < 1 &&
+                  nonPaymentData1.All.ClaimsLobSummary[0].ClaimsNonPaymentRate > 0
+                    ? '< 1%'
+                    : nonPaymentData1.All.ClaimsLobSummary[0].ClaimsNonPaymentRate + '%',
+                color: ['#3381FF', '#D7DCE1'],
+                gdata: ['card-inner', 'claimsNonPaymentRate'],
+                sdata: {
+                  sign: 'up',
+                  data: '+3.7%'
+                }
+              },
+              timeperiod: this.timeFrame
+            };
+          } else {
+            claimsNotPaidRate = {
+              category: 'app-card',
+              type: 'donut',
+              title: null,
+              data: null,
+              timeperiod: null
+            };
+            claimsPaidRate = {
+              category: 'app-card',
+              type: 'donut',
+              title: null,
+              data: null,
+              timeperiod: null
+            };
+          } // end if else
+
           resolve(this.summaryData);
         },
         err => {
