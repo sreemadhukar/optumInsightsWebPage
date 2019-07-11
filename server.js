c726d6041498ec2fa69a68653c4b06717d31d4cf
Bobbiganipalli, Balaji Srikar <balaji.bobbiganipalli@optum.com>
2019-07-11 15:34:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(express.static(path.join(__dirname, '.')));
 
 var apiProxy = httpProxy.createProxyServer();
 var apiForwardingUrl = 'https://gateway-stage-core.optum.com';
-var sessionSecret = 'STwHkLYUwN1L5rc3yqdkuthRvczrBupc';
-var key = 'Q9gRpXWjVm5GXethNxG60utGMGW7NpsO';
+var sessionSecret = '7dX03633CEuFJaf25ot5HlSPOZYQ6E9Y';
+var key = 'PvU8koWDqgbqZNin5aBj00RtRHWze7pC';
 
 app.all("/uhci/prd2/*", function (req, res) {
     apiProxy.web(req, res, { target: apiForwardingUrl, changeOrigin: true, secure: false },
