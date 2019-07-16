3e5c63bb34f6f9faa4e591655d2c1889c749ca44
Vashista <ivashist@ms.ds.uhc.com>
2019-07-17 00:16:38 +0530

    working on prior auth defect for tooltip

--- src/app/common-utils/d3-objects/bar-chart/bar-chart.component.ts
+++ src/app/common-utils/d3-objects/bar-chart/bar-chart.component.ts
@@ -250,14 +250,14 @@ export class BarChartComponent implements OnInit, AfterViewInit {
               .transition()
               .duration(10)
               .style('opacity', 1);
-            div.style('left', d3.event.layerX - 219 + 'px').style('top', d3.event.layerY - 110 + 'px');
+            div.style('left', d3.event.layerX + 'px').style('top', d3.event.layerY - 110 + 'px');
           })
           .on('mousemove', function(d) {
             div
               .transition()
               .duration(10)
               .style('opacity', 1);
-            div.style('left', d3.event.layerX - 219 + 'px').style('top', d3.event.layerY - 110 + 'px');
+            div.style('left', d3.event.layerX + 'px').style('top', d3.event.layerY - 110 + 'px');
           })
           .on('mouseleave', function(d) {
             div
