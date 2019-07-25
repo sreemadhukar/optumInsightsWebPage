d7b0df118b5fd4396d91b5cc69408323fb308c86
Bobbiganipalli, Balaji Srikar <balaji.bobbiganipalli@optum.com>
2019-07-25 14:52:36 -0500

    Update environment.externalstage.ts

--- src/environments/environment.externalstage.ts
+++ src/environments/environment.externalstage.ts
@@ -8,7 +8,7 @@ export const environment: any = {
   apiUrls: {
     ExecutiveSummaryPath: 'providersystems/',
     ProviderSystemClaimsAgg: 'agg-claims/search/v2.0/',
-    ProviderSystemClaimsSummary: 'claims/search/v2.0/',
+    ProviderSystemClaimsSummary: 'claim-metrics/',
     ProviderSystemPriorAuth: 'PriorAuthSummary/',
     SsoTokenPath: 'myinsightOptumIdHandshake',
     SsoRedirectUri:
