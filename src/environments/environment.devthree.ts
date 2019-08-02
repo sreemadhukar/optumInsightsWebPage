5f44135ebb0ae34bb67895b91330eee155fc14ff
Bobbiganipalli, Balaji Srikar <balaji.bobbiganipalli@optum.com>
2019-08-02 11:46:21 -0500

    Update environment.devthree.ts

--- src/environments/environment.devthree.ts
+++ src/environments/environment.devthree.ts
@@ -5,7 +5,7 @@
  * MAINTAIN ALPHABETIC ORDER
  */
 export const environment: any = {
-  apiProxyUrl: 'https://gateway-stage-core.optum.com/api/devthree/pdr/uhci/v1/',
+  apiProxyUrl: 'https://gateway-stage-core.optum.com/api/devfour/pdr/uhci/v1/',
   originUrl: '/',
   apiUrls: {
     ExecutiveSummaryPath: 'providersystems/',
