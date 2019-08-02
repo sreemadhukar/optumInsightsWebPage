96e85856dfafe3489b76e66d2131adfa656873de
mounika <mounikag@optum.com>
2019-08-02 19:13:23 +0530

    Fixed KC formula issue

--- src/app/shared/prior-authorization/prior-auth.service.spec.ts
+++ src/app/shared/prior-authorization/prior-auth.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { PriorAuthService } from './prior-auth.service';
+import { PriorAuthSharedService } from './prior-auth.service';
 
 describe('PriorAuthService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: PriorAuthService = TestBed.get(PriorAuthService);
+    const service: PriorAuthSharedService = TestBed.get(PriorAuthSharedService);
     expect(service).toBeTruthy();
   });
 });
