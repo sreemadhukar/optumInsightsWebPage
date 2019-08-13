b650a8f5d322b2157f44e181aab7dc9355ef06b9
Vashista <ivashist@ms.ds.uhc.com>
2019-08-14 01:26:14 +0530

    working on print overview

---
+++ src/app/components/overview-page/p-overview/p-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-p-overview',
+  templateUrl: './p-overview.component.html',
+  styleUrls: ['./p-overview.component.scss']
+})
+export class POverviewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
