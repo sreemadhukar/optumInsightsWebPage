92d96bcd3e98fe47512b2bd8b1c59a1b2d5a30cf
Dunna <ldunna@LAMU02YR898LVCG.uhc.com>
2019-08-26 12:18:33 -0500

    Creating KOP page container

---
+++ src/app/components/know-your-provider/kop-overview/kop-overview.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EventEmitterService } from 'src/app/shared/know-your-provider/event-emitter.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-kop-overview',
+  templateUrl: './kop-overview.component.html',
+  styleUrls: ['./kop-overview.component.scss']
+})
+export class KopOverviewComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  constructor(private eventEmitter: EventEmitterService) {}
+
+  ngOnInit() {
+    this.eventEmitter.emitEvent(true);
+  }
+
+  ngOnDestroy(): void {
+    this.eventEmitter.emitEvent(false);
+  }
+}
