72ce7036246f7fc6ddb107c474ebdd2642cd9d94
Ranjith_Ankam <akuma659@ms.ds.uhc.com>
2019-09-03 16:35:30 +0530

    enhancements in session timeut msg

--- src/app/auth/login-stub/login-stub.component.ts
+++ src/app/auth/login-stub/login-stub.component.ts
@@ -31,7 +31,7 @@ export class LoginStubComponent implements OnInit {
   isKop = false;
   showWarning = false;
   sessionTimedoutMessage: any = {
-    note: 'Due to inactivity, we have logged out.',
+    note: 'Due to inactivity, we have logged you out.',
     message: 'To return to your UHC Insights, please sign in below.'
   };
   @ViewChild('errorDialog') errorDialog: TemplateRef<any>;
