490b7297a4451804a57c99b828a23395ba4ebd7b
Vashista <ivashist@ms.ds.uhc.com>
2019-09-09 18:12:21 +0530

    working on advocate overview

--- src/app/components/advocate/overview-advocate/overview-advocate.component.ts
+++ src/app/components/advocate/overview-advocate/overview-advocate.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StorageService } from '../../../shared/storage-service.service';
+import { SessionService } from '../../../shared/session.service';
 @Component({
   selector: 'app-overview-advocate',
   templateUrl: './overview-advocate.component.html',
   styleUrls: ['./overview-advocate.component.scss']
 })
 export class OverviewAdvocateComponent implements OnInit {
-  constructor() {}
+  pageTitle: String = '';
+  pagesubTitle: String = '';
+  userName: String = '';
+  constructor(private checkStorage: StorageService, private session: SessionService) {
+    this.pageTitle = 'Welcome, ' + this.userName;
+    this.pagesubTitle = 'Your Insights at a glance.';
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userName =
+      this.session.sessionStorage('loggedUser', 'LastName') +
+      ' ' +
+      this.session.sessionStorage('loggedUser', 'FirstName');
+  }
 }
