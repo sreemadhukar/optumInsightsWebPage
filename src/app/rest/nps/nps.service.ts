f36457d56f3ace212cf8dd4ad144be46e9527b7f
Mansoor <smansoo4@ms.ds.uhc.com>
2019-09-12 23:45:39 +0530

    nps summary api integration

--- src/app/rest/nps/nps.service.ts
+++ src/app/rest/nps/nps.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class NPSService {
+  public currentUser: any;
+  private authBearer: any;
+  private APP_URL: string = environment.apiProxyUrl;
+  private SERVICE_PATH: string = environment.apiUrls.NPSSummary;
   constructor(private http: HttpClient) {}
-  public getNPSSummary() {
-    return this.http.get('./src/assets/mock-data/nps.json');
+
+  public getNPSSummary({ params }) {
+    if (JSON.parse(sessionStorage.getItem('currentUser'))) {
+      this.currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+      this.authBearer = this.currentUser[0].PedAccessToken;
+      const myHeader = new HttpHeaders({
+        Authorization: 'Bearer ' + this.authBearer,
+        Accept: '*/*',
+        'Content-Type': 'application/json'
+      });
+      const url = this.APP_URL + this.SERVICE_PATH;
+      return this.http.get(url, { params, headers: myHeader }).pipe(
+        map(res => JSON.parse(JSON.stringify(res))),
+        catchError(err => of(JSON.parse(JSON.stringify(err))))
+      );
+    }
   }
 }
