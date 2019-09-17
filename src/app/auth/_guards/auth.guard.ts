61c7babc8d3836e19ef5c5f83de6e9520715bb41
Vashista <ivashist@ms.ds.uhc.com>
2019-09-17 23:33:59 +0530

    role based advocate access

--- src/app/auth/_guards/auth.guard.ts
+++ src/app/auth/_guards/auth.guard.ts
@@ -14,9 +14,23 @@ export class AuthGuard implements CanActivate {
       // logged in so return true
       return true;
     }
-
+    // User who is not advocate can't access the pages related to advocates
+    if (this.router.url.includes('Advocate') && !this.advocateRole()) {
+      this.router.navigate(['/OverviewPage']);
+    }
     // not logged in so redirect to login page with the return url
     this.router.navigate(['/login']);
     return false;
+  }
+  advocateRole(): boolean {
+    let userRoleAdvocate = false;
+    try {
+      if (JSON.parse(sessionStorage.getItem('loggedUser'))) {
+        let userRole;
+        userRole = JSON.parse(sessionStorage.getItem('loggedUser')).UserRole;
+        userRoleAdvocate = userRole.some(item => item.includes('UHCI_Advocate'));
+      }
+    } catch (Error) {}
+    return userRoleAdvocate;
   }
 }
