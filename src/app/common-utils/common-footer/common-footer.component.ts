38789d8091878be9d3a3634a6c8382b63b854b11
Madhukar <tmadhuka@ms.ds.uhc.com>
2019-09-30 21:36:19 +0530

    adding date and old month in the front

--- src/app/common-utils/common-footer/common-footer.component.ts
+++ src/app/common-utils/common-footer/common-footer.component.ts
@@ -14,25 +14,55 @@ export class CommonFooterComponent implements OnInit {
 
   ngOnInit() {
     if (this.timePeriod === 'Last 6 Months') {
-      const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      const monthNames = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+        31,
+        28,
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31
+      ];
       const today = new Date();
       const dd = String(today.getDate()).padStart(2, '0');
       const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
       let mmlast6 = String(today.getMonth() - 5).padStart(2, '0'); // January is 0!
-      let yyyy = today.getFullYear();
+      const yyyy = today.getFullYear();
+      let yyyylast6 = today.getFullYear();
+      let ddlast6 = String(today.getDate()).padStart(2, '0');
       if (parseInt(mm) < 6) {
         mmlast6 = String(today.getMonth() + 12 - 5).padStart(2, '0'); // January is 0!
-        yyyy = today.getFullYear() - 1;
+        yyyylast6 = today.getFullYear() - 1;
+      }
+      if (parseInt(dd) > monthNames[parseInt(mmlast6) + 11]) {
+        ddlast6 = monthNames[parseInt(mmlast6) + 11];
       }
       this.timePeriodFooter =
-        monthNames[parseInt(mm) - 1] +
+        monthNames[parseInt(mmlast6) - 1] +
         ' ' +
-        dd +
+        ddlast6 +
         ',' +
         ' ' +
-        yyyy +
+        yyyylast6 +
         ' - ' +
-        monthNames[parseInt(mmlast6) - 1] +
+        monthNames[parseInt(mm) - 1] +
         ' ' +
         dd +
         ',' +
