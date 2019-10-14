df2a14b6f951ed485a3814db6e29db317acdf8e2
Vashista <ivashist@ms.ds.uhc.com>
2019-10-14 19:52:58 +0530

    working on peromacne perloading

--- src/app/head/custom-preloading.ts
+++ src/app/head/custom-preloading.ts
@@ -4,7 +4,7 @@ import { flatMap } from 'rxjs/operators';
 export class CustomPreloadingStrategy implements PreloadingStrategy {
   preload(route: Route, load: Function): Observable<any> {
     const loadRoute = delay => (delay ? timer(5000).pipe(flatMap(_ => load())) : load());
-    if (((route || {}).data || {}).preload) {
+    if (route && route.hasOwnProperty('data') && route.data.hasOwnProperty('preload') && route.data.preload) {
       console.log(
         'Prelaod Path ' + route.path + '. preload : ' + route.data['preload'] + '. delay : ' + route.data['delay']
       );
