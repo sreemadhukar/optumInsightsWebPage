2e1dc57f9285c72adf2ebc677b7dfa73f8e52817
Vashista, Inderjeet <inderjeet_vashista@optum.com>
2019-10-15 18:06:26 +0530

    Update custom-preloading.ts

--- src/app/head/custom-preloading.ts
+++ src/app/head/custom-preloading.ts
@@ -3,7 +3,7 @@ import { Observable, of, timer } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
 export class CustomPreloadingStrategy implements PreloadingStrategy {
   preload(route: Route, load: Function): Observable<any> {
-    const loadRoute = delay => (delay ? timer(5000).pipe(flatMap(_ => load())) : load());
+    const loadRoute = delay => (delay ? timer(8000).pipe(flatMap(_ => load())) : load());
     if (route && route.hasOwnProperty('data') && route.data.hasOwnProperty('preload') && route.data.preload) {
       console.log(
         'Prelaod Path ' + route.path + '. preload : ' + route.data['preload'] + '. delay : ' + route.data['delay']
