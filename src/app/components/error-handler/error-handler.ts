651a7e8ec754e875fc0bcc4846f78e8b7294913b
Vinod Kumar Ravuri <rkuma321@ms.ds.uhc.com>
2019-10-18 17:59:46 +0530

    Sentry Environmnets

---
+++ src/app/components/error-handler/error-handler.ts
@@ -0,0 +1,11 @@
+import * as Raven from 'raven-js';
+import { ErrorHandler } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+Raven.config(environment.sentryKey).install();
+
+export class RavenErrorHandler implements ErrorHandler {
+  handleError(err: any): void {
+    Raven.captureException(err);
+  }
+}
