415ad0f32ce7bd45bb5de51e445fe1091506e3b0
Ranjith_Ankam <akuma659@ms.ds.uhc.com>
2019-10-25 16:02:10 +0530

    refresh token completed UI code

--- src/app/rest/interceptor/http-interceptor.service.ts
+++ src/app/rest/interceptor/http-interceptor.service.ts
@@ -1,58 +1,35 @@
 /** Created By  - Ranjith kumar Ankam - 06-June-2019 **/
 
-import { Injectable } from '@angular/core';
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpResponse,
-  HttpHandler,
-  HttpEvent,
-  HttpErrorResponse
-} from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { map, catchError, retry } from 'rxjs/operators';
+import { Injectable, EventEmitter } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpInterceptorService implements HttpInterceptor {
-  constructor() {}
+  protected emitter = new EventEmitter<boolean>();
+  refreshtoken = false;
+
+  constructor(public http: HttpClient) {
+    this.emitter.pipe().subscribe(text => (this.refreshtoken = text));
+  }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // if (auth.hasAuthorization()) {
-    //   return this.getRequestWithAuthentication(request, next, auth);
-    // } else if (auth.hasAuthorizationRefresh() && request.url !== AUTHORIZE_URL) {
-    //   return auth.refreshToken().flatMap(
-    //     (res: any) => {
-    //       auth.saveTokens(res);
-    //       return this.getRequestWithAuthentication(request, next, auth);
-    //     }
-    //   ).catch(() => {
-    //     return next.handle(request);
-    //   });
-    // } else if (request.url === AUTHORIZE_URL) {
-    //   return next.handle(request);
-    // }
+    if (this.refreshtoken) {
+      return next.handle(request);
+    }
+    if (!this.isTokenExpired(request)) {
+      this.emitter.emit(false);
+      return this.getRequestWithAuthentication(request, next);
+    } else if (this.isTokenExpired(request) && !this.refreshtoken) {
+      this.emitter.emit(true);
 
-    return this.getRequestWithAuthentication(request, next);
+      // API call to get new refresh token & save in session
 
-    // return next.handle(request).pipe(
-    //   retry(2),
-    //   map((event: HttpEvent<any>) => {
-    //     /*if (event instanceof HttpResponse) {
-    //       console.log('event--->>>', event);
-    //     }*/
-    //     return event;
-    //   }),
-    //   catchError((error: HttpErrorResponse) => {
-    //     /*let data = {};
-    //     data = {
-    //       reason: error && error.error.reason ? error.error.reason : '',
-    //       status: error.status
-    //     };*/
-    //     return throwError(error);
-    //   })
-    // );
+      return this.getRequestWithAuthentication(request, next);
+    }
   }
 
   private getRequestWithAuthentication(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -71,5 +48,44 @@ export class HttpInterceptorService implements HttpInterceptor {
     }
     request = request.clone({ headers: request.headers.set('Accept', '*/*') });
     return next.handle(request);
+  }
+
+  private isTokenExpired(request: HttpRequest<any>): boolean {
+    const currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+    let token = '';
+    if (currentUser && request.url.indexOf(environment.originUrl + 'api/getHeac') === -1) {
+      token =
+        !environment.internalAccess && environment.production
+          ? currentUser[0].AccessToken
+          : currentUser[0].PedAccessToken;
+    }
+
+    // token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJp
+    // MGpLWVJwdGtmNTNkTDdNWlhjdEhRQ0ZraTRESUdOS3d6NTFKRis1Ymw5TWlWOW
+    // VMRkVHRzBsMTJjaFlnaElaNy85YjhJSktwUmUvUTQyR1hUa1hJVi9KeklnazZMM
+    // HhEa3cxR05VVURsQWFOMlVORWkzaS9sWm5DWlpoYjJHSGw1ZGp3N1lHczZoa0hOb
+    // VBiSjZnYnNiYjRqenRlVE9UVnFjUWZ6K3J2Lzg9IiwiaXNzIjoiUTlnUnBYV2pWb
+    // TVHWGV0aE54RzYwdXRHTUdXN05wc08iLCJleHAiOjE1NzE2NzU4ODcsImF1dGhvcm
+    // l0aWVzIjoiW1JPTEVfVVNFUl0ifQ.CDTyKhyxkesfw96jWR_yxvz5BCiLp7sM7Vif
+    // HcEhyZ4`;
+
+    if (token) {
+      const date = this.getTokenExpirationDate(token);
+      if (date === undefined) {
+        return false;
+      }
+      return !(date.valueOf() > new Date().valueOf());
+    }
+    return false;
+  }
+
+  private getTokenExpirationDate(token: string): Date {
+    const decoded = jwt_decode(token);
+    if (decoded.exp === undefined) {
+      return null;
+    }
+    const date = new Date(0);
+    date.setUTCSeconds(decoded.exp);
+    return date;
   }
 }
