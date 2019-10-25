b3dd5df0b64f85e15247a00a0b0e33ec0e4f954c
Madhukar <tmadhuka@ms.ds.uhc.com>
2019-10-25 16:05:33 +0530

    card fotter date changes

--- src/app/common-utils/common-footer/common-footer.component.ts
+++ src/app/common-utils/common-footer/common-footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CommonFooterComponent implements OnInit {
   @Input() timePeriod: String;
+  @Input() title: String;
 
   timePeriodFooter: String = null;
 
@@ -29,22 +30,30 @@ export class CommonFooterComponent implements OnInit {
         { monthNames: 'Dec', monthlastdate: 31 }
       ];
       const today = new Date();
-      const dd = String(today.getDate()).padStart(2, '0');
-      const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
-      let mmlast6 = String(today.getMonth() - 5).padStart(2, '0'); // January is 0!
+      let mm = String(today.getMonth()).padStart(2, '0'); // January is 0!
+      if (parseInt(mm) === 0) {
+        mm = '12';
+      }
+      let mmlast6 = String(today.getMonth() - 6).padStart(2, '0'); // January is 0!
       const yyyy = today.getFullYear();
       let yyyylast6 = today.getFullYear();
-      let ddlast6 = String(today.getDate()).padStart(2, '0');
       if (yyyylast6 % 4 === 0) {
         month[1].monthlastdate = 29;
       }
       if (parseInt(mm) < 6) {
-        mmlast6 = String(today.getMonth() + 12 - 5).padStart(2, '0'); // January is 0!
+        mmlast6 = String(today.getMonth() + 12 - 6).padStart(2, '0'); // January is 0!
         yyyylast6 = today.getFullYear() - 1;
       }
-      if (parseInt(dd) > month[parseInt(mmlast6) - 1].monthlastdate) {
-        ddlast6 = String(month[parseInt(mmlast6) - 1].monthlastdate);
+      const dd = String(month[parseInt(mm) - 1].monthlastdate);
+      //   let dd = String(today.getDate()).padStart(2, '0');
+      // let ddlast6 = String(today.getDate()).padStart(2, '0');
+      const ddlast6 = String(month[parseInt(mmlast6) - 1].monthlastdate);
+      if (this.title.includes('Prior')) {
+        console.log('madhukar');
       }
+      // if (parseInt(dd) > month[parseInt(mmlast6) - 1].monthlastdate) {
+      //   ddlast6 = String(month[parseInt(mmlast6) - 1].monthlastdate);
+      // }
       this.timePeriodFooter =
         month[parseInt(mmlast6) - 1].monthNames +
         ' ' +
