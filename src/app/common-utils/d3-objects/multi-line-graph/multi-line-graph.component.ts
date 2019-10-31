b4c546de8cb870b1a25f60f04d8d05cdd1e112f9
Kumar <skuma395@LAMU02TC0MNGTFM.uhc.com>
2019-10-31 08:51:54 -0500

    update

--- src/app/common-utils/d3-objects/multi-line-graph/multi-line-graph.component.ts
+++ src/app/common-utils/d3-objects/multi-line-graph/multi-line-graph.component.ts
@@ -366,7 +366,7 @@ export class MultiLineGraphComponent implements OnInit {
     const yScale = d3
       .scaleLinear()
       .domain([0, highestValue]) // input
-      .range([180, 0])
+      .range([90, 0])
       .nice(3); // output
 
     // const ydata = [];
