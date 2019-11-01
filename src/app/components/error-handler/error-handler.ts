70a3b60642af70d53d9165026849f520b1b6e035
Vashista <ivashist@ms.ds.uhc.com>
2019-11-02 00:53:47 +0530

    removed sentry.io

--- src/app/components/error-handler/error-handler.ts
+++ src/app/components/error-handler/error-handler.ts
@@ -1,11 +1,11 @@
-import * as Raven from 'raven-js';
-import { ErrorHandler } from '@angular/core';
-import { environment } from '../../../environments/environment';
+// import * as Raven from 'raven-js';
+// import { ErrorHandler } from '@angular/core';
+// import { environment } from '../../../environments/environment';
 
-Raven.config(environment.sentryKey).install();
+// Raven.config(environment.sentryKey).install();
 
-export class RavenErrorHandler implements ErrorHandler {
-  handleError(err: any): void {
-    Raven.captureException(err);
-  }
-}
+// export class RavenErrorHandler implements ErrorHandler {
+//   handleError(err: any): void {
+//     Raven.captureException(err);
+//   }
+// }
