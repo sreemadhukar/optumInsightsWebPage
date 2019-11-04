5b12abb1b3393a27642379b34ce206f23057034d
Mansoor <smansoo4@ms.ds.uhc.com>
2019-11-04 19:25:33 +0530

    filter reset on logout

--- src/app/shared/kop/kop.service.ts
+++ src/app/shared/kop/kop.service.ts
@@ -11,40 +11,6 @@ import { Reimbursement } from './kop.class.reimbursement';
   providedIn: 'root'
 })
 export class KOPSharedService {
-  filters: any = [
-    {
-      title: 'Last Completed Quarter',
-      selected: false,
-      default: false,
-      quarterFormat: 'default',
-      timeFrameFormat: 'Quarter and Year',
-      filters: ['LAST_COMPLETED_QUARTER']
-    },
-    {
-      title: 'Year To Date',
-      selected: false,
-      default: false,
-      timeFrameFormat: 'Year',
-      quarterFormat: 'default',
-      filters: ['YEAR_TO_DATE']
-    },
-    {
-      title: 'Quarter over Quarter',
-      selected: true,
-      default: true,
-      timeFrameFormat: 'Quarter vs Quarter',
-      quarterFormat: 'default',
-      filters: ['QUARTER_OVER_QUARTER']
-    },
-    {
-      title: 'Total Last Year',
-      selected: false,
-      default: false,
-      timeFrameFormat: 'Last Year',
-      quarterFormat: 'YTD',
-      filters: ['YEAR_TO_DATE', 'TOTAL_LAST_YEAR']
-    }
-  ];
   constructor(private kopService: KopService) {}
 
   public getSummary(params: any) {
