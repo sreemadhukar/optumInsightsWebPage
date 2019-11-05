85c0290855b88b0b1e788ffeb3e4ac1e29982534
Ranjith_Ankam <akuma659@ms.ds.uhc.com>
2019-11-05 15:19:06 +0530

    sepearting PCOR and Prior Auth in hamburger menu

--- src/app/head/hamburger-menu/hamburger-menu.component.ts
+++ src/app/head/hamburger-menu/hamburger-menu.component.ts
@@ -95,11 +95,12 @@ export class HamburgerMenuComponent implements AfterViewInit, OnInit, OnDestroy,
         }
       ]
     },
-    {
-      icon: 'care-delivery',
-      name: 'Care Delivery',
-      children: [{ name: 'Prior Authorizations', path: '/CareDelivery/priorAuth' }]
-    },
+    // {
+    //   icon: 'care-delivery',
+    //   name: 'Care Delivery',
+    //   children: [{ name: 'Prior Authorizations', path: '/CareDelivery/priorAuth' }]
+    // },
+    { icon: 'care-delivery', name: 'Prior Authorizations', path: '/CareDelivery/priorAuth', disabled: false },
     {
       icon: 'service-interaction',
       name: 'Service Interaction',
@@ -333,11 +334,48 @@ export class HamburgerMenuComponent implements AfterViewInit, OnInit, OnDestroy,
   bar PCOR will be hidden
   */
   insertPCORnav() {
-    if (!this.navCategories[2].children.some(i => i.name === 'Patient Care Opportunity')) {
-      this.navCategories[2].children.push({
-        name: 'Patient Care Opportunity',
-        path: '/CareDelivery/PatientCareOpportunity'
-      });
+    // if (!this.navCategories[2].children.some(i => i.name === 'Patient Care Opportunity')) {
+    //   this.navCategories[2].children.push({
+    //     name: 'Patient Care Opportunity',
+    //     path: '/CareDelivery/PatientCareOpportunity'
+    //   });
+    // }
+    if (!this.navCategories.some(i => i.name === 'Patient Care Opportunity')) {
+      this.navCategories[4] = {
+        icon: 'care-delivery',
+        name: 'Patient Care Opportunity ',
+        path: '/CareDelivery/PatientCareOpportunity',
+        disabled: false
+      };
+      this.navCategories[5] = {
+        icon: 'service-interaction',
+        name: 'Service Interaction',
+        children: [
+          { name: 'Self Service', path: '/ServiceInteraction/SelfService' },
+          { name: 'Calls', path: '/ServiceInteraction/Calls' }
+        ]
+      };
+      this.navCategories[6] = {
+        icon: 'timeline',
+        name: 'Summary Trends',
+        path: '/AdminSummaryTrends',
+        disabled: true
+      };
+    } else {
+      this.navCategories[4] = {
+        icon: 'service-interaction',
+        name: 'Service Interaction',
+        children: [
+          { name: 'Self Service', path: '/ServiceInteraction/SelfService' },
+          { name: 'Calls', path: '/ServiceInteraction/Calls' }
+        ]
+      };
+      this.navCategories[5] = {
+        icon: 'timeline',
+        name: 'Summary Trends',
+        path: '/AdminSummaryTrends',
+        disabled: true
+      };
     }
   }
   checkToggle(bool: boolean) {
