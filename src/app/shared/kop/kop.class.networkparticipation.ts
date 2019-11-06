af5081fb7377c649a5d3fb3e08ede4ac5aeaa665
Dhiman <adhiman4@ms.ds.uhc.com>
2019-11-06 16:52:45 +0530

    Put label for the Onborading and issueResoultion tiles

--- src/app/shared/kop/kop.class.networkparticipation.ts
+++ src/app/shared/kop/kop.class.networkparticipation.ts
@@ -57,6 +57,7 @@ export class NetworkParticipation {
         caption: 'Avg. turnaround time',
         singleCard: this.singleCard,
         report: false,
+        showCaption: true,
         color: ['#3381FF', '#80B0FF'],
         sdata: {
           sign: 'up',
@@ -72,6 +73,7 @@ export class NetworkParticipation {
         singleCard: this.singleCard,
         caption: 'Providers onboarded',
         report: false,
+        showCaption: true,
         color: ['#3381FF', '#80B0FF'],
         sdata: {
           sign: 'up',
