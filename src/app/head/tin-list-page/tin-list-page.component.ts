d00a90c5bd095d52dcba127d20bc8b376d2244cd
Madhukar <tmadhuka@ms.ds.uhc.com>
2019-11-08 16:13:18 +0530

    adding angular table in Tin List Page

--- src/app/head/tin-list-page/tin-list-page.component.ts
+++ src/app/head/tin-list-page/tin-list-page.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SessionService } from '../../shared/session.service';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tin-list-page',
@@ -9,11 +11,29 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./tin-list-page.component.scss']
 })
 export class TinListPageComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  tinsDisplayedColumns: string[] = ['Tin', 'Tinname'];
   providerName: string;
-  numberOfTins: string;
+  numberOfTins: any;
   tinsData: any;
   selectedtins: any;
   constructor(private iconRegistry: MatIconRegistry, private session: SessionService, sanitizer: DomSanitizer) {
+    this.session.getTins().then(data => {
+      this.tinsData = data;
+      for (let i = 0; i < this.tinsData.length; i++) {
+        if (this.tinsData[i].Tinname === 'TIN Name Not Found') {
+          this.tinsData[i].Tinname = 'Tax ID Name Not Available';
+        }
+        this.numberOfTins = this.tinsData.length;
+      }
+      this.paginator._intl.itemsPerPageLabel = 'Display';
+      this.paginator._intl.getRangeLabel = function(page, pageSize, length) {
+        return 'Page ' + Math.floor(page + 1) + ' of ' + Math.floor(length / pageSize + 1);
+      };
+      this.selectedtins = new MatTableDataSource(this.tinsData);
+      this.selectedtins.paginator = this.paginator;
+      // this.selectedtins.paginator._intl.firstPageLabel = 'Display';
+    });
     iconRegistry.addSvgIcon(
       'backButton',
       sanitizer.bypassSecurityTrustResourceUrl('/src/assets/images/TIN-List-Back-Button-Icon.svg')
@@ -25,10 +45,6 @@ export class TinListPageComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.numberOfTins = '40';
-    this.session.getTins().then(data => {
-      this.tinsData = data;
-      this.selectedtins = this.tinsData;
-    });
+    this.providerName = this.session.getHealthCareOrgName();
   }
 }
