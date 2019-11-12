c5a0564a9eef0fc8ccd63f7eb26f07250fd40da7
Ranjith_Ankam <akuma659@ms.ds.uhc.com>
2019-11-12 15:24:10 +0530

    refresh token final code

--- src/app/auth/_service/internal.service.ts
+++ src/app/auth/_service/internal.service.ts
@@ -34,7 +34,7 @@ export class InternalService {
         map(
           user => {
             if (typeof user !== 'undefined' && user !== null) {
-              console.log(user);
+              // console.log(user);
               this.tempUser = user;
               this.sso = [];
               this.sso.push({
