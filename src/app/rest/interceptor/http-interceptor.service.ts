adb43f7f92662e74f3df01b7f7e5008f1ebad648
Bobbiganipalli, Balaji Srikar <balaji.bobbiganipalli@optum.com>
2019-11-27 14:57:43 -0600

    Update http-interceptor.service.ts

--- src/app/rest/interceptor/http-interceptor.service.ts
+++ src/app/rest/interceptor/http-interceptor.service.ts
@@ -67,7 +67,7 @@ export class HttpInterceptorService implements HttpInterceptor {
         });
       }
     }
-    if (request.url.indexOf('myinsightOptumIdHandshake') !== -1 || request.url.indexOf('ldapauth') !== -1) {
+    if (request.url.indexOf('myinsightOptumIdHandshake') === -1 || request.url.indexOf('ldapauth') === -1) {
       request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
     }
     request = request.clone({ headers: request.headers.set('Accept', '*/*') });
