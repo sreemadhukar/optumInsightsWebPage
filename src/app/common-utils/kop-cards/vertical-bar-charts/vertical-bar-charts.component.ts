8db1951012692b8297d1298514d1cf228b2da117
Mansoor <mansoor.shaik@optum.com>
2019-12-10 20:36:10 +0530

    vertical bar issue

--- src/app/common-utils/kop-cards/vertical-bar-charts/vertical-bar-charts.component.ts
+++ src/app/common-utils/kop-cards/vertical-bar-charts/vertical-bar-charts.component.ts
@@ -9,6 +9,7 @@ export class VerticalBarChartsComponent implements OnInit {
   @Input() data: any;
 
   public heights: string[] = ['0%', '0%'];
+  public marginTops: string[] = ['0%', '0%'];
   public get barChartData() {
     return this.data;
   }
@@ -19,10 +20,15 @@ export class VerticalBarChartsComponent implements OnInit {
     const [{ title: value1 }, { title: value2 }] = this.data;
     if (value1 < value2) {
       this.heights[1] = divisionValue + '%';
+      this.marginTops[1] = '0%';
       this.heights[0] = (value1 / value2) * divisionValue + '%';
+      this.marginTops[0] = 85 - parseInt(this.heights[0].replace('%', '')) + '%';
     } else {
       this.heights[1] = (value2 / value1) * divisionValue + '%';
+      this.marginTops[1] = 85 - parseInt(this.heights[1].replace('%', '')) + '%';
+
       this.heights[0] = divisionValue + '%';
+      this.marginTops[0] = '0%';
     }
   }
 }
