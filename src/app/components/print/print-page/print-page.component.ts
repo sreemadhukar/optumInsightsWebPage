891daf54c1293a56cb055e56b914a53b85e6b3c5
Vashista <ivashist@ms.ds.uhc.com>
2019-12-13 17:03:15 +0530

    working on load time

--- src/app/components/print/print-page/print-page.component.ts
+++ src/app/components/print/print-page/print-page.component.ts
@@ -11,15 +11,33 @@ export class PrintPageComponent implements OnInit {
   selectedPage;
   printStyle = true;
   pagename = '';
-
+  data: any;
   constructor() {
     this.printStyle = true;
+    this.data = [
+      { page: 'overviewPage', load: 7000 },
+      { page: 'gettingReimbursedSummary', load: 7000 },
+      { page: 'paymentsPage', load: 5000 },
+      { page: 'appealsPage', load: 4000 },
+      { page: 'nonPaymentsPage', load: 6000 },
+      { page: 'paymentIntegrityPage', load: 3000 },
+      { page: 'priorAuthPage', load: 4000 },
+      { page: 'pcorPage', load: 4000 },
+      { page: 'callsPage', load: 4000 }
+    ];
   }
 
   ngOnInit() {
-    this.currentPage.subscribe(currentPage => (this.selectedPage = currentPage));
-    setTimeout(() => {
-      (window as any).print();
-    }, 7000);
+    this.currentPage.subscribe(c => (this.selectedPage = c));
+    setTimeout(
+      () => {
+        (window as any).print();
+      },
+      this.data.flatMap(i => (i.page === this.selectedPage ? i.load : 0))
+    );
+    console.log(
+      'laod Time for Print',
+      this.data.flatMap(i => (i.page === this.selectedPage ? i.load : 0))
+    );
   }
 }
