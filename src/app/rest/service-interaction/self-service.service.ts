3a427bbfa2607fbbd82a29fc1bd42d31ae145b10
Vashista <ivashist@ms.ds.uhc.com>
2020-01-28 01:04:15 +0530

    added interface

--- src/app/rest/service-interaction/self-service.service.ts
+++ src/app/rest/service-interaction/self-service.service.ts
@@ -3,7 +3,67 @@ import { environment } from '../../../environments/environment';
 import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
 import { ServiceInteractionModule } from '../../components/service-interaction/service-interaction.module';
 import { map, catchError } from 'rxjs/operators';
-import { combineLatest, of } from 'rxjs';
+import { combineLatest, of, Observable } from 'rxjs';
+
+interface SelfServiceI {
+  ProviderSysKey: number;
+  HealthCareOrganizationName: string;
+  RecordRefreshDate: string;
+  ReportingPeriod: string;
+  SelfServiceInquiries: {
+    ALL: {
+      SelfService: {
+        AverageClaimCount: number;
+        AverageClaimProcessingTime: number;
+        AveragePaperReconsideredProcessingTime: number;
+        AveragePaperClaimProcessingTime: number;
+        AverageReconsideredProcessingTime: number;
+        ReconsideredProcessingTimeDifferential: number;
+        ClaimProcessingTimeDifferential: number;
+        AveragePaperReconsideredCount: number;
+        EligibilityCallCount: number;
+        AveragePaperClaimCount: number;
+        BenefitsCallCount: number;
+        TotalCallTime: number;
+        PhoneCallTime: number;
+        SelfServiceCallTime: number;
+        TotalCallCost: number;
+        TotalPhoneCost: number;
+        TotalSelfServiceCost: number;
+        TotalCallCount: number;
+        ReduceClaimCost: number;
+        ClaimPhoneCost: number;
+        SelfServicePhoneCost: number;
+        TotalClaimCallCount: number;
+        ReduceEligibilityAndBenefitsCost: number;
+        EligibilityAndBenefitPhoneCost: number;
+        EligibilityAndBenefitSelfServiceCost: number;
+        EligibilityAndBenefitCallCount: number;
+        ReducePriorAuthorizationsCost: number;
+        PriorAuthorizationsPhoneCost: number;
+        PriorAuthorizationsSelfServiceCost: number;
+        AuthCallCount: number;
+      };
+      Utilizations: {
+        LinkAdoptionRate: number;
+        OverallLinkAdoptionRate: number;
+        PaperAndPostageAdoptionRate: number;
+        PaperAndPostageElectronicWeighting: number;
+        PaperAndPostagePaperWeighting: number;
+        SelfServiceCallsAndClaimsWeighting: number;
+        SizeOfOpportunityCallsAndClaimsWeighting: number;
+        SizeOfOpportunityCallsAndClaimsPaperAndPostageWeighting: number;
+      };
+      RecordRefreshDate: String;
+      ReportingPeriodStartDate: String;
+      ReportingPeriodEndDate: String;
+    };
+  };
+  ResolvingIssues: Object;
+  PriorAuth: string;
+  PatientCareOpportunity: string;
+}
+
 @Injectable({ providedIn: ServiceInteractionModule })
 export class SelfServiceService {
   public currentUser: any;
@@ -13,7 +73,7 @@ export class SelfServiceService {
   private EXECUTIVE_SERVICE_PATH: string = environment.apiUrls.ExecutiveSummaryPath;
   constructor(private http: HttpClient) {}
 
-  public getSelfServiceData(...parameters) {
+  public getSelfServiceData(...parameters): Observable<SelfServiceI[]> {
     this.currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
     this.authBearer = this.currentUser[0].PedAccessToken;
     const myHeader = new HttpHeaders({
