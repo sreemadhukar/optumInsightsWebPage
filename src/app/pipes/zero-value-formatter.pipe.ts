d3c68a6c47c0b3ac8e0471baeb3182ea3a05af96
Vashista <ivashist@ms.ds.uhc.com>
2020-01-31 20:50:58 +0530

    created pipe

---
+++ src/app/pipes/zero-value-formatter.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'zeroValueFormatter'
+})
+export class ZeroValueFormatterPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    const temp: number = +value;
+    if (temp < 1) {
+      return '< 1';
+    } else {
+      return value;
+    }
+
+    return null;
+  }
+}
