aa565790727b9b0c4c0b10d71a27aa7c855dc9fb
Vashista <ivashist@ms.ds.uhc.com>
2020-02-06 14:41:54 +0530

    working on self Service

--- src/app/pipes/zero-value-formatter.pipe.ts
+++ src/app/pipes/zero-value-formatter.pipe.ts
@@ -1,15 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import { CommonUtilsService } from '../shared/common-utils.service';
 @Pipe({
   name: 'zeroValueFormatter'
 })
 export class ZeroValueFormatterPipe implements PipeTransform {
-  transform(value: any, args?: any): any {
+  constructor(private common: CommonUtilsService) {}
+  transform(value: any, args?: any): string {
     const temp: number = +value;
     if (temp < 1) {
       return '< 1';
     } else {
-      return value;
+      return this.common.nondecimalFormatter(temp);
     }
   }
 }
