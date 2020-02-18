9647579b4b2f6c9754b3d9cb8493bc428f9ce948
Ranjith_Ankam <ranjith.ankam@optum.com>
2020-02-18 19:30:18 +0530

    contact us in-progress code

--- src/app/head/contactus/contactus.component.ts
+++ src/app/head/contactus/contactus.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactus.component.scss']
 })
 export class ContactusComponent implements OnInit {
+  barChartsArray = [
+    { question: 'QUESTION1', answer: 'ANSWER1' },
+    { question: 'QUESTION2', answer: 'ANSWER2' },
+    { question: 'QUESTION3', answer: 'ANSWER3' },
+    { question: 'QUESTION4', answer: 'ANSWER4' },
+    { question: 'QUESTION5', answer: 'ANSWER5' }
+  ];
   constructor() {}
 
   ngOnInit() {}
