f2df9e671133f36427af14d775934fc0df8a626d
Bakkolla <prakash_bakkolla@optum.com>
2020-02-25 17:28:08 +0530

    Modified PCOR Link

--- src/app/components/care-delivery-page/patient-care-opportunity/patient-care-opportunity.component.ts
+++ src/app/components/care-delivery-page/patient-care-opportunity/patient-care-opportunity.component.ts
@@ -233,7 +233,6 @@ export class PatientCareOpportunityComponent implements OnInit {
         });
     });
     this.reportTitle = 'View the PCOR Report';
-    this.reportText = 'View in-depth details of your health system in the PCOR report. ';
     this.reportLink = 'View the Patient Care Opportunity Report';
   }
   PCORreport() {
