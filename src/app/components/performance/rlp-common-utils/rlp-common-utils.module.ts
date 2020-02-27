3b41700ce3b9dae0fbe6469b924f14ebdd915fbe
Vashista <inderjeet_vashista@optum.com>
2020-02-27 23:51:30 +0530

    create common table component

--- src/app/components/performance/rlp-common-utils/rlp-common-utils.module.ts
+++ src/app/components/performance/rlp-common-utils/rlp-common-utils.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RlpCommonBarComponent } from './d3/rlp-common-bar/rlp-common-bar.component';
+import { RlpTableComponent } from './rlp-table/rlp-table.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [RlpCommonBarComponent],
-  exports: [RlpCommonBarComponent]
+  declarations: [RlpCommonBarComponent, RlpTableComponent],
+  exports: [RlpCommonBarComponent, RlpTableComponent]
 })
 export class RlpCommonUtilsModule {}
