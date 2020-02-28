d69ae79f9410b9b492f429458a4b2b33e7f09f28
Vashista <inderjeet_vashista@optum.com>
2020-02-28 23:14:12 +0530

    working on table footer

--- src/app/components/performance/rlp-common-utils/rlp-table/rlp-table.component.ts
+++ src/app/components/performance/rlp-common-utils/rlp-table/rlp-table.component.ts
@@ -8,28 +8,32 @@ import { rlpData } from '../../../../modals/rlp-data';
 export class RlpTableComponent implements OnInit {
   public qTinSearch: string;
   public qGroupNameSearch: string;
-  private tableData: any;
-  private pageNumber: number;
-  private selectPageSize: string;
-  private startIndex;
-  private endIndex;
-  private totalPages: number;
-  private pageSize = ['2', '4', '6', '8', '25'];
+  public tableData: any;
+  public currentPageNumber: number;
+  public selectPageSize: string;
+  public startIndex;
+  public endIndex;
+  public totalPages: number;
+  public pageSize = ['2', '4', '6', '8', '25'];
   constructor() {}
 
   ngOnInit() {
-    this.setPagination(this.pageSize[0], 0, this.pageSize[0], 1, rlpData.data.length / this.pageSize.length);
+    this.selectPageSize = this.pageSize[0];
+    this.totalPages = Math.ceil(rlpData.data.length / +this.selectPageSize);
+    this.setPagination(0, 1);
     this.tableData = rlpData.data;
-    console.log(this.tableData);
-    console.log(this.qTinSearch, this.qGroupNameSearch);
   }
-  setPagination(pageSize, startIndex, endIndex, pageNumber, totalPages) {
-    this.selectPageSize = pageSize;
+  setPagination(startIndex, currentPageNumber) {
     this.startIndex = startIndex;
-    this.endIndex = endIndex;
-    this.pageNumber = pageNumber;
-    this.totalPages = totalPages;
+    this.currentPageNumber = currentPageNumber;
   }
-  prevClick() {}
-  nextClick() {}
+  pageSizeMethod() {
+    this.totalPages = Math.ceil(rlpData.data.length / +this.selectPageSize);
+  }
+  prevClick() {
+    this.setPagination(this.startIndex - +this.selectPageSize, --this.currentPageNumber);
+  }
+  nextClick() {
+    this.setPagination(this.startIndex + +this.selectPageSize, ++this.currentPageNumber);
+  }
 }
