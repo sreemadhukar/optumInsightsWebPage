2b3b1656c110aab688aac0c7d1e15d47b5ff0c8c
Vashista <inderjeet_vashista@optum.com>
2020-02-29 19:22:29 +0530

    added rlp-body

--- src/app/components/performance/rlp-common-utils/rlp-common-utils.module.ts
+++ src/app/components/performance/rlp-common-utils/rlp-common-utils.module.ts
@@ -7,10 +7,11 @@ import { RlpTableComponent } from './rlp-table/rlp-table.component';
 import { RlpTableItemComponent } from './rlp-table/rlp-table-item/rlp-table-item.component';
 import { PipesModule } from './../../../pipes/pipes.module';
 import { MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { RlpBodyComponent } from './rlp-body/rlp-body.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, PipesModule, MatFormFieldModule, MatInputModule, MatSelectModule],
-  declarations: [RlpCommonBarComponent, RlpTableComponent, RlpTableItemComponent, RlpHeaderComponent],
-  exports: [RlpCommonBarComponent, RlpTableComponent, RlpTableItemComponent, RlpHeaderComponent]
+  declarations: [RlpCommonBarComponent, RlpTableComponent, RlpTableItemComponent, RlpHeaderComponent, RlpBodyComponent],
+  exports: [RlpCommonBarComponent, RlpTableComponent, RlpTableItemComponent, RlpHeaderComponent, RlpBodyComponent]
 })
 export class RlpCommonUtilsModule {}
