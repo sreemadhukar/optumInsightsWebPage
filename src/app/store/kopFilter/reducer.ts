469578091d663e6de1c2d933cc21596c48d529e9
Dhiman <aman_dhiman@optum.com>
2020-03-09 17:16:55 +0530

    Add session storage in KOP Filter

--- src/app/store/kopFilter/reducer.ts
+++ src/app/store/kopFilter/reducer.ts
@@ -1,22 +1,17 @@
 import { APPLY_KOP_FILTER, RESET_KOP_FILTER } from './actions';
 import { FILTER_MASTER_DATA } from './kopFilterMasterData';
 import { find as _find } from 'lodash';
+import { loadKOPState } from '../filter/localStorage';
 
 // Get default filter
 const defaultFilter = _find(FILTER_MASTER_DATA, { default: true });
-
-// Initialze the initial state
-export const INITIAL_STATE = {
-  timePeriod: defaultFilter['title'],
-  ...defaultFilter
-};
 
 /**
  * Reducer function handle the state update of KOP FILTER
  * @param state  Current state if not provided INITIAL_STATE
  * @param action Dispatched action to process the state
  */
-export function KopFilterReducer(state = INITIAL_STATE, action) {
+export function KopFilterReducer(state = loadKOPState(), action) {
   switch (action.type) {
     case APPLY_KOP_FILTER:
       return {
