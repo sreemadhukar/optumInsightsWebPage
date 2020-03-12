d0148633045eea36e9869332565fbded9fe9bc55
Rajan <rashmi.rajan@optum.com>
2020-03-12 19:56:11 +0530

    rest call added contd

--- src/app/rest/performance/performance-rest.service.ts
+++ src/app/rest/performance/performance-rest.service.ts
@@ -16,13 +16,10 @@ export class PerformanceRestService {
     return this.http.get('./src/assets/mock-data/performance.json');
   }
 
-  public getNetworkLeversData(parameters) {
-    let tparams = {};
-    if (parameters) {
-      tparams = {
-        TimeFilter: 'YTD'
-      };
-    }
+  public getNetworkLeversData(...parameters) {
+    let tparams = new HttpParams();
+    tparams = tparams.append('TimeFilter', 'YTD');
+
     const URL = this.APP_URL + this.NETWORK_LEVER_PATH + parameters[0] + parameters[1];
     console.log('URL-------------->', URL);
     return this.http.get(URL, { params: tparams }).pipe(
--- src/app/shared/performance/summary-shared.service.ts
+++ src/app/shared/performance/summary-shared.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@angular/core';
+import { CommonUtilsService } from '../common-utils.service';
+import { SessionService } from '../session.service';
+import { AuthorizationService } from '../../auth/_service/authorization.service';
+import { GlossaryMetricidService } from '../glossary-metricid.service';
+import { PerformanceRestService } from '../../rest/performance/performance-rest.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SummarySharedService {
-  constructor() {}
+  public timeFrame;
+  public tin;
+  public lob;
+  public providerKey;
+
+  constructor(
+    private MetricidService: GlossaryMetricidService,
+    private common: CommonUtilsService,
+    private session: SessionService,
+    private toggle: AuthorizationService,
+    private performanceRestService: PerformanceRestService
+  ) {}
+
+  getParameterCategories(param) {
+    let parameters = [];
+    this.providerKey = this.session.providerKeyData();
+    parameters = [this.providerKey];
+    return parameters;
+  }
+
+  public referralsShared(param) {
+    console.log('Hiii Referals2');
+    this.timeFrame = this.common.getTimePeriodFilterValue(param.timePeriod);
+    // this.lob = param.lineOfBusiness ? _.startCase(param.lineOfBusiness.toLowerCase()) : 'All';
+    this.providerKey = this.session.providerKeyData();
+    return new Promise(resolve => {
+      const parameters = this.getParameterCategories(param);
+      parameters[1] = '?requestType=SPECIALIST_REFERRAL_HCO';
+      this.performanceRestService.getNetworkLeversData(...parameters).subscribe(
+        networkReferralsData => {
+          resolve(networkReferralsData);
+          console.log('networkReferralsData', networkReferralsData);
+        },
+        err => {
+          console.log('Advocate Page , Error for Payment cards', err);
+        }
+      );
+    });
+  }
 }
