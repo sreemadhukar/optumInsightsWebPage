aa9f4f45f649b4e6a8b1334cf557eca31336daca
Ranjith_Ankam <ranjith.ankam@optum.com>
2020-04-02 19:04:42 +0530

    Single LOB donut change

--- src/app/auth/login-stub/login-stub.component.ts
+++ src/app/auth/login-stub/login-stub.component.ts
@@ -127,9 +127,8 @@ export class LoginStubComponent implements OnInit {
 
       this.authService.getJwt().subscribe(data => {
         sessionStorage.setItem('token', JSON.stringify(data['token']));
-        this.internalService.getPublicKey();
       });
-
+      this.internalService.getPublicKey();
       // }
     } else {
       if (this.route.queryParams) {
