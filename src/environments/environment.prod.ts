8d55dd7c0978282e8c18d06ea7159c0874f02120
Madhukar <madhukar_thota@optum.com>
2020-04-09 14:09:05 +0530

    disabling new-PI page in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -58,8 +58,8 @@ export const environment: any = {
     PaymentsBySubmission: 'claim-submissions/v2.0/',
     GroupPremiumDesignation: 'group-premium-designation/',
     NetworkLever: 'network-lever/',
-    PaymentIntegrityTabsInfo: 'pi-tab-info/',
-    NewPaymentIntegrity: 'payment-integrity/'
+    PaymentIntegrityTabsInfo: 'pi-tab-info/'
+    // NewPaymentIntegrity: 'payment-integrity/' // please uncomment this when new-PI i allowed in prod
   },
   headerKeys: {
     selectedCompany: '',
