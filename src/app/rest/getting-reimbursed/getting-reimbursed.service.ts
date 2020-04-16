59c258e72d57356ada6f54120a78b8d04aeee6a0
Dhiman <aman_dhiman@optum.com>
2020-04-16 14:25:47 +0530

    Handle Resturcture Response and change Endpoint

--- src/app/rest/getting-reimbursed/getting-reimbursed.service.ts
+++ src/app/rest/getting-reimbursed/getting-reimbursed.service.ts
@@ -5,6 +5,7 @@ import { environment } from '../../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { combineLatest, of } from 'rxjs';
+import { get as _get } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
@@ -34,18 +35,6 @@ export class GettingReimbursedService {
     );
   }
 
-  /*  public appealsData(...parameters) {
-    const appealsParams = parameters[1];
-    if (!appealsParams.Tin) {
-      appealsParams.AllProviderTins = true;
-    }
-    const appealsURL = this.APP_URL + this.APPEALS_SERVICE_PATH + parameters[0];
-    return this.http.post(appealsURL, appealsParams).pipe(
-      map(res => JSON.parse(JSON.stringify(res))),
-      catchError(err => of(JSON.parse(JSON.stringify(err))))
-    );
-  }*/
-
   /** function for Appeals PDP api */
   public claimsAppealsData(...parameters) {
     // const appealsParam = parameters[1];
@@ -63,7 +52,10 @@ export class GettingReimbursedService {
     }
     const appealsURL = this.APP_URL + this.APPEALS_SERVICE + parameters[0] + appealsReqType;
     return this.http.post(appealsURL, appealsParam).pipe(
-      map(res => JSON.parse(JSON.stringify(res))),
+      map(res => {
+        const dataValue = _get(res, ['Data', '0'], []);
+        return dataValue;
+      }),
       catchError(err => of(JSON.parse(JSON.stringify(err))))
     );
   }
@@ -78,7 +70,10 @@ export class GettingReimbursedService {
     }
     const appealsURL = this.APP_URL + this.APPEALS_OVERTURN + parameters[0] + appealsReqType;
     return this.http.post(appealsURL, appealsParam).pipe(
-      map(res => JSON.parse(JSON.stringify(res))),
+      map(res => {
+        const dataValue = _get(res, ['Data', '0'], []);
+        return dataValue;
+      }),
       catchError(err => of(JSON.parse(JSON.stringify(err))))
     );
   }
