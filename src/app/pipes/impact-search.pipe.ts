1bf74ad84c4eb5870e9ec8a43331a04e098a5036
Vashista <inderjeet_vashista@optum.com>
2020-04-24 01:51:01 +0530

    working on impact assignment

--- src/app/pipes/impact-search.pipe.ts
+++ src/app/pipes/impact-search.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ImpactSearchPipe implements PipeTransform {
   transform(value: any, args?: any): any {
-    console.log('value, args', args);
+    console.log('args', args);
     return value;
   }
 }
