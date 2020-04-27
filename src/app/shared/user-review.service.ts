eae4a02794dc5a176eec70810dd5f6dbd3a43f93
Ranjith_Ankam <ranjith.ankam@optum.com>
2020-04-27 12:25:29 +0530

    user review interceptor code

--- src/app/shared/user-review.service.ts
+++ src/app/shared/user-review.service.ts
@@ -5,19 +5,12 @@ declare const externalRatingIntercept: any;
   providedIn: 'root'
 })
 export class UserReviewService {
-  abc: any;
   constructor(private cookieService: CookieService) {}
 
   removeCreatedCookies() {
-    this.abc = document.getElementById('checking');
-    console.log(this.abc);
-    if (this.abc) {
-      this.abc.replaceWith('');
-    }
     this.cookieService.delete('UZ_TI_dc_value');
     this.cookieService.delete('UZ_TI_S_0E30EBC3BF5CEA11AA60BF52C2E54AD8');
     this.cookieService.delete('s_sq');
-    this.cookieService.set('UZ_TI_dc_value', '0');
     externalRatingIntercept();
   }
 }
