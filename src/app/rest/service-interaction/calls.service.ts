77cf01f99fef3b73dce56cb8720d4372af38eea0
Vinod Kumar Ravuri <ravuri_vinod@optum.com>
2020-04-28 19:37:49 +0530

    removing unused imports

--- src/app/rest/service-interaction/calls.service.ts
+++ src/app/rest/service-interaction/calls.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, catchError, retry } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class CallsService {
