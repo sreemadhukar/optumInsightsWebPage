68ed9e42b3437d24ec6f5fef13f5bf4decad4039
Dunna <ldunna@LAMU02YR898LVCG.uhc.com>
2020-05-21 14:48:58 -0500

    Fix

--- src/app/components/advocate/health-system-details/health-system-details.component.ts
+++ src/app/components/advocate/health-system-details/health-system-details.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HealthSystemDetailsSharedService } from '../../../shared/advocate/health-system-details-shared.service';
 import { StorageService } from '../../../shared/storage-service.service';
-import { Router } from '@angular/router';
 import { GroupPremiumDesignationService } from './../../../rest/group-premium-designation/group-premium-designation.service';
 import { CreatePayloadService } from '../../../shared/uhci-filters/create-payload.service';
 import { NgRedux, select } from '@angular-redux/store';
@@ -31,7 +30,6 @@ export class HealthSystemDetailsComponent implements OnInit {
     private healthSystemService: HealthSystemDetailsSharedService,
     private readonly groupPremiumDesignationService: GroupPremiumDesignationService,
     private checkStorage: StorageService,
-    private readonly router: Router,
     private readonly common: CommonUtilsService,
     private readonly createPayloadService: CreatePayloadService,
     private readonly ngRedux: NgRedux<IAppState>,
