9c53ad70e8a786314ff4dd7edae837b8bd359194
Rajan <rashmi.rajan@optum.com>
2020-06-01 18:46:16 +0530

    conflicts resolved

--- src/app/head/hamburger-menu/hamburger-menu.component.ts
+++ src/app/head/hamburger-menu/hamburger-menu.component.ts
@@ -107,9 +107,9 @@ export class HamburgerMenuComponent implements AfterViewInit, OnInit, OnDestroy 
         {
           name: 'Payment Integrity',
           children: [
-            { name: 'Medical Records Coding Review', path: '/GettingReimbursed/PaymentIntegrity' }
+            { name: 'Medical Records Coding Review', path: '/GettingReimbursed/PaymentIntegrity' },
             // Uncomment Next Line when data is available for Smart Edits
-            // { name: 'Smart Edits', path: '/GettingReimbursed/SmartEdits' }
+            { name: 'Smart Edits', path: '/GettingReimbursed/SmartEdits' }
           ]
         }
       ],
